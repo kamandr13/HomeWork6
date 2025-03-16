5bbe1e1f1c19f8844dba76757abca4ffbcc5a97b
kamandr13 <kamandr@yandex.ru>
2025-03-17 00:30:10 +0300

    + задача 10

--- src/Main.java
+++ src/Main.java
@@ -49,5 +49,11 @@ public class Main {
             total = total + contribution;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
+        System.out.println();
+
+        int num = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + "*" + i + "=" + (num * i));
+        }
     }
 }
